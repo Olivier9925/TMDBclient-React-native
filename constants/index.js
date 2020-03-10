1c5cf4a1aad528eddfeaae312cdf9b924266d858
Olivier ALLARD JACQUIN <oallardjacquin@gmail.com>
2020-03-10 10:38:07 +0100

    FEAT: style constant

--- constants/index.js
+++ constants/index.js
@@ -1,3 +1,5 @@
 import colors from './colorConstants';
+import styles from '@constants/stylesConstants';
 
 export const colorConstants = colors;
+export const stylesConstants = styles;
---
+++ constants/stylesConstants.js
@@ -0,0 +1,17 @@
+import colorConstants from '@constants/colorConstants';
+
+const navigationBarVisibleHeight = 66;
+
+export default {
+    style1: { // Height from visible part of the navigation bar (visible:66 + invisible:30 = 96 see TabBarComponent)
+        padding: 20,
+        flex: 1,
+        backgroundColor: colorConstants.BACK_SECOND
+    },
+    style2: {
+        height: 42,
+        width: 130,
+        marginTop: 68,
+    },
+    }
+};
