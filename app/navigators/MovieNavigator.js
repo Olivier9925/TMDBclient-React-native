705dd0e8a623d634f0580ef786c066e19b50fbea
Olivier Allard Jacquin <oallardjacquin@gmail.com>
2020-05-28 16:01:55 +0200

    Nettoyage MovieNavigator

--- app/navigators/MovieNavigator.js
+++ app/navigators/MovieNavigator.js
@@ -1,51 +1,23 @@
 import React from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { useDispatch, useSelector } from 'react-redux';
-import { createStackNavigator } from '@react-navigation/stack';
-import { colorConstants } from '@constants';
-import { logout } from '@actions/userActions';
+import {Dimensions} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
 import Movie from '@scenes/Movie';
-import Home from '@scenes/Home';
-import Signup from '@scenes/Signup';
-import Connexion from '@scenes/Connexion';
 import MovieList from '@scenes/MovieList';
 
 const Stack = createStackNavigator();
 
 let width = Dimensions.get('window').width;
 
-const MovieNavigator = () =>
-{
-	const dispatch = useDispatch();
-	const connexion = useSelector(state => state.userReducer.connexion);
-
-	return (
-		<Stack.Navigator
-			initialRouteName={Movie}
-			screenOptions={{
-				headerShown: false,
-			}}>
-			<Stack.Screen name={'Movie list'} component={MovieList} />
-			<Stack.Screen name={'Movie'} component={Movie} />
-		</Stack.Navigator>
-	);
+const MovieNavigator = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName={Movie}
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen name={'Movie list'} component={MovieList} />
+      <Stack.Screen name={'Movie'} component={Movie} />
+    </Stack.Navigator>
+  );
 };
 export default MovieNavigator;
-
-const styles = StyleSheet.create({
-	statusBar: {
-		width: width - 100,
-		marginLeft: 100,
-		flex: 1,
-		flexDirection: 'row',
-		justifyContent: 'flex-end',
-		alignItems: 'center',
-		alignContent: 'center',
-	},
-	logText: {
-		color: colorConstants.TEXT,
-		padding: 10,
-	},
-});
-
